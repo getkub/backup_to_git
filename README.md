1692914eb9d86170d416d185726b0e35de22acfa
getkub <kinbsk@gmail.com>
2021-01-30 16:51:32 +0000

    Updated with backup files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Script to tar files as per a list and archive to same repository
 
 ## How  to run
 - Update directory (recursive onwards) in configs/backup_directories.list one entry per line
-- Run   cd script ;  ./clone_backup_push.sh
+- Run   cd bin ;  ./clone_backup_push.sh
 - Validate if copies are present in git repository
 
 
---
+++ bin/clone_backup_push.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+####################################
+#
+# Clone git, backup files & upload to git
+#
+####################################
+
+# What to backup. 
+backup_list="../configs/backup_directories.list"
+thisHost=$(hostname -s)
+thisHost="tempHost"
+
+# Where to backup to.
+dest="../backups/${thisHost}/"
+mkdir -p $dest
+
+# Create archive filename.
+for abs_dir in `grep -v '#' ${backup_list}`
+do 
+    dname=`basename $abs_dir`
+    echo "backup_dir=$abs_dir dest_file=$dest/$dname"
+    tar czf $dest/${dname}.tgz $abs_dir
+done
+# Long listing of files in $dest to check file sizes.
+#ls -lh $dest
+
+git add -A && git commit -am "Updated with backup files"
+git push origin
+
+echo "Git push finished"
+
